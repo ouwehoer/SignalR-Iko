54ac14d0f8b62bbec56156cbe8620e258ef66d24
Pim Keulers <p.keulers@nieuwestroom.nl>
2024-01-22 08:50:12 +0100

    created chat example

---
+++ SignalR-server/Hubs/ChatHub.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public class ChatHub
+    : Hub
+{
+    public ChatHub(ILogger<ChatHub> logger)
+    {
+        Logger = logger;
+    }
+
+    public ILogger<ChatHub> Logger { get; set; }
+
+    public async Task SendMessage(
+        string chatName,
+        string username,
+        string message)
+    {
+        await Clients
+            .Group(chatName)
+            .SendAsync("ReceiveChat",username, message);
+    }
+
+    public async Task AddToChat(
+        string chatName,
+        string userName)
+    {
+        // function below creates group if not existant
+        // if ConnectionID already in group, function does nothing
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatName);
+
+        await SendMessage(chatName, userName, $"is connected");
+    }
+
+    public async Task RemoveFromChat(
+        string chatName)
+    {
+        // function below creates group if not existant
+        // if ConnectionID already in group, function does nothing
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatName);
+    }
+
+    public async override Task OnConnectedAsync()
+    {
+        Logger.LogInformation($"Connected Client {Context.ConnectionId}");
+        await base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (exception != null)
+        {
+            Logger.LogError($"{nameof(ChatHub)}.{nameof(OnDisconnectedAsync)} got an Exception: {exception.Message}");
+        }
+        else
+        {
+            Logger.LogInformation($"Disconnected Client {Context.ConnectionId}");
+        }
+        return base.OnDisconnectedAsync(exception);
+    }
+}
--- SignalR-server/Program.cs
+++ SignalR-server/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddLogging()
+    .AddCors()
+    .AddSignalR()
+    ;
+
+var app = builder.Build();
+
+app.UseCors(builder =>
+    builder
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    );
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
